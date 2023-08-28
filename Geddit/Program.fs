978a0f3be0346b0b8c65cef7e56e8af169c1c3f1
Aistis Raulinaitis <sheganinans@gmail.com>
2023-08-28 00:43:51 -0700

    more sync

--- Geddit/Program.fs
+++ Geddit/Program.fs
@@ -13,13 +13,36 @@ Async.Sleep 5_000 |> Async.RunSynchronously
 let startDay = DateTime (2018, 01, 01)
 let endDay = DateTime (2023, 08, 01)
 
+let TOTAL_DAYS = (endDay-startDay).Days
+
+type private SyncFinished = class end
+
 let finishedMailbox = MailboxProcessor.Start (fun inbox ->
-  use sw = File.AppendText "finished.txt"
   async {
-    let! (root : string) = inbox.Receive ()
-    sw.WriteLine root
-    sw.Flush ()
+    while true do
+      let! (root : string) = inbox.Receive ()
+      lock typeof<SyncFinished> (fun () ->
+        use sw = File.AppendText "finished.txt"
+        sw.WriteLine root
+        sw.Flush ())
+      Directory.Delete root
   })
+
+type private SyncCounter = class end
+
+let counterMailbox = MailboxProcessor.Start (fun inbox ->
+  let mutable m = Map.empty
+  async {
+    while true do
+      let! (root : string) = inbox.Receive ()
+      lock typeof<SyncCounter> (fun () ->
+        m <- m |> Map.change root (function | None -> Some 1 | Some n -> Some (n + 1))
+        let c = m |> Map.find root
+        printfn $"c: {c}, {TOTAL_DAYS}"
+        if TOTAL_DAYS = c
+        then finishedMailbox.Post root)
+    }
+  )
 
 let finished =
   try File.ReadLines "finished.txt" |> Set.ofSeq
@@ -31,39 +54,35 @@ let go () =
   | Result.Ok roots ->
     roots
     |> Seq.filter (not << finished.Contains)
-    |> Seq.iter (fun root ->
+    |> PSeq.withDegreeOfParallelism 8
+    |> PSeq.iter (fun root ->
       Directory.CreateDirectory root |> ignore
       discord.SendNotification $"starting: {root}" |> Async.Start
       let mutable trySet =
-        seq { 0..(endDay-startDay).Days }
+        seq { 0..(endDay-startDay).Days - 1 }
         |> Seq.map (startDay.AddDays << float)
         |> Set.ofSeq
 
-      let mutable noData = []
+      let mutable noDataAcc = []
         
       while trySet.Count <> 0 do
-        printfn $"dates left: {trySet.Count}"
         let res =
           trySet
-          |> PSeq.withDegreeOfParallelism 8
-          |> PSeq.map (fun day ->
-            printfn $"req: {root} {day}"
-            day, StockTradeQuotes.reqAndConcat root day |> Async.RunSynchronously)
-          |> PSeq.cache
-        let data = res |> PSeq.filter (function | _, RspStatus.Ok _ -> true | _ -> false)
-        let discons = res |> PSeq.filter (fun (_, r) -> r = RspStatus.Disconnected)
-        let errs = res |> PSeq.filter (function | _, RspStatus.Err _ -> true | _ -> false)
-        async {
-          data |> PSeq.iter
-            (function
-            | day, RspStatus.Ok data ->
-              printfn $"save: {root} {day}"
-              StockTradeQuotes.saveData root day data
-            | _ -> raise (Exception "UNEXP1: This should never happen."))
-        } |> Async.Start
+          |> Seq.map (fun day -> day, StockTradeQuotes.reqAndConcat root day |> Async.RunSynchronously)
+          |> Seq.cache
+        let data = res |> Seq.filter (function | _, RspStatus.Ok _ -> true | _ -> false)
+        let discons = res |> Seq.filter (fun (_, r) -> r = RspStatus.Disconnected)
+        let errs = res |> Seq.filter (function | _, RspStatus.Err _ -> true | _ -> false)
+        let noData = res |> Seq.filter (fun (_, r) -> r = RspStatus.NoData)
+        data |> Seq.iter
+          (function
+          | day, RspStatus.Ok data ->
+            counterMailbox.Post root
+            StockTradeQuotes.saveData root day data
+          | _ -> raise (Exception "UNEXP1: This should never happen."))
 
-        noData <- List.append noData (res |> PSeq.toList |> List.filter (fun (_, r) -> r = RspStatus.NoData))
-        trySet <- discons |> PSeq.map fst |> Set.ofSeq |> Set.union (errs |> Seq.map fst |> Set.ofSeq)
+        noDataAcc <- List.append noDataAcc (noData |> List.ofSeq)
+        trySet <- discons |> Seq.map fst |> Set.ofSeq |> Set.union (errs |> Seq.map fst |> Set.ofSeq)
         
         if trySet.Count <> 0
         then
@@ -74,17 +93,13 @@ let go () =
       (
         let noDataFile = $"{root}.nodata.txt"
         use sw = new StreamWriter (noDataFile)
-        noData |> List.iter (fun d -> sw.WriteLine (d.ToString ()))
+        noDataAcc |> List.iter (fun d ->
+          counterMailbox.Post root
+          sw.WriteLine (d.ToString ()))
         sw.Close ()
         Wasabi.uploadFile noDataFile StockTradeQuotes.BUCKET $"{root}/nodata.txt"
         File.Delete noDataFile
-      )
-    
-      async {
-        while (Directory.GetFiles root).Length <> 0 do do! Async.Sleep 100
-        finishedMailbox.Post root
-        Directory.Delete root
-      } |> Async.Start)
+      ))
     
 try go ()
 with err ->
