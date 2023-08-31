f095ca7ab817749a2c157a2e12ac862d43f9b847
Aistis Raulinaitis <sheganinans@gmail.com>
2023-08-30 19:00:04 -0700

    buf streams

--- Geddit/StockTradeQuotes.fs
+++ Geddit/StockTradeQuotes.fs
@@ -81,31 +81,35 @@ let saveData (symbol : string) (date : DateTime) (data : Data) =
   let fileName = $"{symbol}/%04i{date.Year}-%02i{date.Month}-%02i{date.Day}.parquet.lz4"
   using (new MemoryStream ())
     (fun ms ->
-      use f = new ParquetFileWriter (ms, cols, leaveOpen = true)
-      use rowGroup = f.AppendRowGroup ()
-      use w = rowGroup.NextColumn().LogicalWriter<DateTime> () in w.WriteBatch data.TimeOfTrade
-      use w = rowGroup.NextColumn().LogicalWriter<int> () in w.WriteBatch data.Sequence
-      use w = rowGroup.NextColumn().LogicalWriter<uint> () in w.WriteBatch data.Size
-      use w = rowGroup.NextColumn().LogicalWriter<uint16> () in w.WriteBatch data.Condition
-      use w = rowGroup.NextColumn().LogicalWriter<float> () in w.WriteBatch data.Price
-      use w = rowGroup.NextColumn().LogicalWriter<DateTime> () in w.WriteBatch data.TimeOfQuote
-      use w = rowGroup.NextColumn().LogicalWriter<uint> () in w.WriteBatch data.BidSize
-      use w = rowGroup.NextColumn().LogicalWriter<float> () in w.WriteBatch data.Bid
-      use w = rowGroup.NextColumn().LogicalWriter<byte> () in w.WriteBatch data.BidExchange
-      use w = rowGroup.NextColumn().LogicalWriter<uint> () in w.WriteBatch data.AskSize
-      use w = rowGroup.NextColumn().LogicalWriter<float> () in w.WriteBatch data.Ask
-      use w = rowGroup.NextColumn().LogicalWriter<byte> () in w.WriteBatch data.AskExchange
-      using (new MemoryStream ())
-        (fun out ->
-          let settings = LZ4EncoderSettings ()
-          settings.CompressionLevel <- LZ4Level.L03_HC
-          using (LZ4Stream.Encode (out, settings, leaveOpen = true))
-            (fun lz ->
-              ms.Seek (0, SeekOrigin.Begin) |> ignore
-              ms.CopyTo lz
-              lz.Flush ())
-          out.Seek (0, SeekOrigin.Begin) |> ignore
-          Wasabi.uploadStream out BUCKET fileName))
+      using (new BufferedStream (ms))
+        (fun buf ->
+          use f = new ParquetFileWriter (buf, cols, leaveOpen = true)
+          use rowGroup = f.AppendRowGroup ()
+          use w = rowGroup.NextColumn().LogicalWriter<DateTime> () in w.WriteBatch data.TimeOfTrade
+          use w = rowGroup.NextColumn().LogicalWriter<int> () in w.WriteBatch data.Sequence
+          use w = rowGroup.NextColumn().LogicalWriter<uint> () in w.WriteBatch data.Size
+          use w = rowGroup.NextColumn().LogicalWriter<uint16> () in w.WriteBatch data.Condition
+          use w = rowGroup.NextColumn().LogicalWriter<float> () in w.WriteBatch data.Price
+          use w = rowGroup.NextColumn().LogicalWriter<DateTime> () in w.WriteBatch data.TimeOfQuote
+          use w = rowGroup.NextColumn().LogicalWriter<uint> () in w.WriteBatch data.BidSize
+          use w = rowGroup.NextColumn().LogicalWriter<float> () in w.WriteBatch data.Bid
+          use w = rowGroup.NextColumn().LogicalWriter<byte> () in w.WriteBatch data.BidExchange
+          use w = rowGroup.NextColumn().LogicalWriter<uint> () in w.WriteBatch data.AskSize
+          use w = rowGroup.NextColumn().LogicalWriter<float> () in w.WriteBatch data.Ask
+          use w = rowGroup.NextColumn().LogicalWriter<byte> () in w.WriteBatch data.AskExchange  
+          using (new MemoryStream ())
+            (fun out ->
+              using (new BufferedStream (ms))
+                (fun buf2 ->
+                  let settings = LZ4EncoderSettings ()
+                  settings.CompressionLevel <- LZ4Level.L03_HC
+                  using (LZ4Stream.Encode (buf2, settings, leaveOpen = true))
+                    (fun lz ->
+                      buf.Seek (0, SeekOrigin.Begin) |> ignore
+                      buf.CopyTo lz
+                      lz.Flush ())
+                  buf2.Seek (0, SeekOrigin.Begin) |> ignore
+                  Wasabi.uploadStream buf2 BUCKET fileName))))
 
 let toReq (root : string) (day : DateTime) =
   let ds = $"%04i{day.Year}%02i{day.Month}%02i{day.Day}"
