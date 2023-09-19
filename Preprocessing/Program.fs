dce2e992c2df5db17a131022bc96a2b5635998f4
Aistis Raulinaitis <sheganinans@gmail.com>
2023-09-19 12:17:27 -0700

    init preprocessing

---
+++ Preprocessing/Program.fs
@@ -0,0 +1,9 @@
+﻿open System
+open ClickHouse.Client.ADO
+open Dapper
+
+let pw = Environment.GetEnvironmentVariable "CK_PW"
+
+using (new ClickHouseConnection ($"Host=127.0.0.1;Password={pw}")) (fun c ->
+  let r = c.QueryAsync<int> "select count() from thetadata_stock_trade_quotes prewhere ticker == 'NVDA'" |> Async.AwaitTask |> Async.RunSynchronously
+  printfn $"{r}"  )
