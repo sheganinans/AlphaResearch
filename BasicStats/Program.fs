18f2c62e0b9efa7738bec2ffbfd673b20016934e
Aistis Raulinaitis <sheganinans@gmail.com>
2023-09-23 23:23:03 -0700

    add count

--- BasicStats/Program.fs
+++ BasicStats/Program.fs
@@ -1,15 +1,37 @@
 ﻿open ClickHouse.Client.Copy
+open Dapper
+
 open Shared.Clickhouse
 
-let i = new ClickHouseBulkCopy (ck)
-i.DestinationTableName <- "quote_1min_agg"
-i.BatchSize <- 100_000
 
-getUniqTickers ()
-|> Seq.iter (fun t ->
-  printfn $"{t}"
-  getLogReturns t 1 Minute
-  |> Seq.map (fun r -> [| box r.ticker; box r.ts; box r.log_ret |])
-  |> i.WriteToServerAsync
-  |> Async.AwaitTask
-  |> Async.RunSynchronously)
+
+let logRet () =
+  let i = new ClickHouseBulkCopy (ck)
+  i.DestinationTableName <- "quote_1min_agg"
+  i.BatchSize <- 100_000
+  getUniqTickers ()
+  |> Seq.iter (fun t ->
+    printfn $"{t}"
+    getLogReturns t 1 Minute
+    |> Seq.map (fun r -> [| box r.ticker; box r.ts; box r.log_ret |])
+    |> i.WriteToServerAsync
+    |> Async.AwaitTask
+    |> Async.RunSynchronously)
+
+let count () =
+  let i = new ClickHouseBulkCopy (ck)
+  i.DestinationTableName <- "ticker_count"
+  i.BatchSize <- 1
+  getUniqTickers ()
+  |> Seq.iter (fun t ->
+    printfn $"{t}"
+    let count = 
+      ck.Query<int> $"select count() from thetadata_stock_trade_quotes prewhere ticker == '{t}'"
+      |> Seq.head
+      |> uint64
+    [|[| box t; box count |]|]
+    |> i.WriteToServerAsync
+    |> Async.AwaitTask
+    |> Async.RunSynchronously)
+  
+count ()
---
+++ Uploader/sql/count.sql
@@ -0,0 +1,4 @@
+create table if not exists ticker_count
+( ticker LowCardinality(String)
+, total UInt64 CODEC (T64, ZSTD(1))
+) engine = MergeTree
